0abd5af8c3ac8fd2f96c5ea693d74f110ad60777
Ankur Dahiya <legalos.lotr@gmail.com>
2016-04-26 12:58:55 +0530

    more changes to build and serve commands

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var webpack = require("webpack");
+
 module.exports = {
   entry: "./src/js/index.js",
   output: {
@@ -12,5 +14,12 @@ module.exports = {
         loader: "babel-loader"
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
+      }
+    })
+  ]
 }
