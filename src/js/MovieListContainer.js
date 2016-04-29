917a99f31570aa1f0e0a5e22b484e2261cc4ae56
Ankur Dahiya <legalos.lotr@gmail.com>
2016-04-29 19:30:38 +0530

    some formatting fixes

--- src/js/MovieListContainer.js
+++ src/js/MovieListContainer.js
@@ -5,11 +5,48 @@ import { connect } from 'react-redux'
 const MovieListContainerImpl = React.createClass({
   render() {
     return (
-      <BootstrapTable data={this.props.movies} search={true}>
-        <TableHeaderColumn dataField='Title' isKey={true} dataSort={true}>Title</TableHeaderColumn>
-        <TableHeaderColumn dataField='Genre' dataSort={true}>Genre</TableHeaderColumn>
+      <BootstrapTable trClassName="movie-list-tr" hover={true} condensed={true} data={this.props.movies} search={true}>
+        <TableHeaderColumn dataAlign='center' dataField='Title' dataFormat={this.formatTitle} isKey={true}>Title</TableHeaderColumn>
+        <TableHeaderColumn
+          dataAlign='center'
+          dataField='imdbRating'
+          dataSort={true}
+          dataFormat={this.formatRating}
+          sortFunc={this.sortRating}
+        >
+          Rating
+        </TableHeaderColumn>
+        <TableHeaderColumn dataAlign='center' dataField='Released'>Released</TableHeaderColumn>
+        <TableHeaderColumn dataAlign='center' dataField='Genre'>Genre</TableHeaderColumn>
+        <TableHeaderColumn dataAlign='center' dataField='Runtime'>Runtime</TableHeaderColumn>
+        <TableHeaderColumn dataAlign='center' dataField='Plot'>Plot</TableHeaderColumn>
       </BootstrapTable>
     )
+  },
+  formatTitle(cell, row) {
+    return (
+      <a href={`http://www.imdb.com/title/${row['imdbID']}`} target="_blank">
+        <img width={100} src={row['Poster']}/>
+        <div><strong>{row['Title']}</strong></div>
+      </a>
+    )
+  },
+  formatRating(cell, row) {
+    return (
+      <div>
+        <p>IMDB: {row['imdbRating']} <br/> <span className="small">{row['imdbVotes']} votes</span></p>
+        <p>Metascore: {row['Metascore']}</p>
+        <p>Awards: {row['Awards']}</p>
+      </div>
+    )
+  },
+  sortRating(a, b, order) {
+    let diff = Number(a['imdbRating']) - Number(b['imdbRating'])
+    if (order == 'desc') {
+      return diff
+    } else {
+      return -diff
+    }
   }
 });
 
